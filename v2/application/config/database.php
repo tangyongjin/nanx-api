b9292c93d9be14c6aa6168917f8858f964585300
alex <tangyongjin@gmail.com>
2023-10-14 23:01:30 +0800

    fix MMenu

--- v2/application/config/database.php
+++ v2/application/config/database.php
@@ -8,7 +8,7 @@ $query_builder = true;
 
 $db['default'] = [
   'dsn' => '',
-  'hostname' => '192.168.5.66',
+  'hostname' => '192.168.5.72',
   'username' => 'root',
   'password' => '123456',
   'database' => 'nanx',
--- v2/application/models/MMenu.php
+++ v2/application/models/MMenu.php
@@ -88,8 +88,8 @@ class MMenu extends CI_Model {
                 boss_portal_menu_list wl 
                 where  parent_id is NULL ORDER BY wl.id";
     $res = $this->db->query($sql)->result_array();
-
-    for ($i = 0; $i < count($res); ++$i) {
+    $len1 = count($res);
+    for ($i = 0; $i < $len1; ++$i) {
       $id = $res[$i]['key'];
       $sql = "select wl.menu_level,wl.datagrid_code,wl.is_association_process,
             wl.process_key, wl.badge,wl.badge_key,
@@ -97,10 +97,10 @@ class MMenu extends CI_Model {
             if(wl.is_leaf='true',true,false ) as  'isLeaf',
             wl.parent_id 
             from  boss_portal_menu_list wl where  wl.parent_id='$id' ORDER BY wl.id";
-      $row = $this->db->query($sql)->result_array();
-
-      for ($j = 0; $j < count($row); ++$j) {
-        $three = $row[$j]['key'];
+      $rows = $this->db->query($sql)->result_array();
+      $len2 = count($rows);
+      for ($j = 0; $j < $len2; ++$j) {
+        $three = $rows[$j]['key'];
         $three_sql = "select wl.menu_level,wl.datagrid_code,wl.is_association_process,
                 wl.process_key,
                 wl.badge,wl.badge_key,wl.id 'key',
@@ -110,9 +110,9 @@ class MMenu extends CI_Model {
                 from  boss_portal_menu_list wl  
                 where   wl.parent_id='$three' ORDER BY wl.id";
         $three_row = $this->db->query($three_sql)->result_array();
-        $row[$j]['children'] = $three_row;
+        $rows[$j]['children'] = $three_row;
       }
-      $res[$i]['children'] = $row;
+      $res[$i]['children'] = $rows;
     }
     return $res;
   }
@@ -122,32 +122,17 @@ class MMenu extends CI_Model {
 
 
   public function stringToBoolean($rows) {
-    //第一级
-    for ($i = 0; $i < count($rows); $i++) {
+    $len = count($rows);
+    for ($i = 0; $i < $len; $i++) {
       $children = $rows[$i]['children'];
-      if ($rows[$i]['isLeaf'] == '0') {
-        $rows[$i]['isLeaf'] = false;
-      }
-      if ($rows[$i]['isLeaf'] == '1') {
-        $rows[$i]['isLeaf'] = true;
-      }
-      //第二级
-      for ($j = 0; $j < count($children); $j++) {
-        if ($children[$j]['isLeaf'] == '0') {
-          $rows[$i]['children'][$j]['isLeaf'] = false;
-        }
-        if ($children[$j]['isLeaf'] == '1') {
-          $rows[$i]['children'][$j]['isLeaf'] = true;
-        }
-        //第三级
+      $rows[$i]['isLeaf'] == '0' ? $rows[$i]['isLeaf'] = false : $rows[$i]['isLeaf'] = true;
+      $len2 = count($children);
+      for ($j = 0; $j < $len2; $j++) {
+        $children[$j]['isLeaf'] == '0' ? $rows[$i]['children'][$j]['isLeaf'] = false : $rows[$i]['children'][$j]['isLeaf'] = true;
         $grandchildren = $children[$j]['children'];
-        for ($x = 0; $x < count($grandchildren); $x++) {
-          if ($grandchildren[$x]['isLeaf'] == '0') {
-            $rows[$i]['children'][$j]['children'][$x]['isLeaf'] = false;
-          }
-          if ($grandchildren[$x]['isLeaf'] == '1') {
-            $rows[$i]['children'][$j]['children'][$x]['isLeaf'] = true;
-          }
+        $len3 = count($grandchildren);
+        for ($x = 0; $x < $len3; $x++) {
+          $grandchildren[$x]['isLeaf'] == '0' ? $rows[$i]['children'][$j]['children'][$x]['isLeaf'] = false : $rows[$i]['children'][$j]['children'][$x]['isLeaf'] = true;
         }
       }
     }
@@ -155,7 +140,9 @@ class MMenu extends CI_Model {
   }
 
   public function getParentText($res) {
-    for ($i = 0; $i < count($res); ++$i) {
+    $len = count($res);
+    for ($i = 0; $i < $len; ++$i) {
+
       $id = $res[$i]['parent_id'];
       if ('' != $id || null != $id) {
         $sql = "select text as  parent_text from boss_portal_menu_list WHERE id = '$id' ";
